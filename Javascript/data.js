3f0fb0a104f2adaebae94bf05521946bfd2c3fe1
Ananya <kh.ananya.025@gmail.com>
2023-08-24 23:57:17 +0530

    Added

---
+++ Javascript/data.js
@@ -0,0 +1,145 @@
+// const data = [
+//     {
+//         id :1,
+//         imgsrc : "https://w7.pngwing.com/pngs/183/70/png-transparent-iphone-3gs-iphone-4-telephone-others-electronics-gadget-mobile-phone.png",
+//         title : "I Phone 4", 
+//         desc : "Rs 12000"
+//     },
+//     {
+//         id :2,
+//         imgsrc : "https://w7.pngwing.com/pngs/122/753/png-transparent-iphone-5s-iphone-5c-iphone-6-telephone-handphone-electronics-gadget-mobile-phone-case.png",
+//         title : "I Phone 5 ", 
+//         desc : "Rs 14500 "
+//     },
+//     {
+//         id :3,
+//         imgsrc : "https://w7.pngwing.com/pngs/308/123/png-transparent-iphone-6-plus-iphone-6s-plus-apple-iphone-6s-apple-gadget-electronics-mobile-phone.png",
+//         title : "I Phone 6 ",
+//         desc : "Rs 16000 "
+//     },
+//     {
+//         id :4,
+//         imgsrc : "https://e7.pngegg.com/pngimages/488/251/png-clipart-iphone-7-plus-apple-telephone-unlocked-4g-apple-iphone-gadget-electronics.png",
+//         title : "I Phone 7 ",
+//         desc : "Rs 16500 "
+//     },
+//     {
+//         id :5,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :6,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :7,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :8,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :9,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :10,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :11,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :12,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :13,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :14,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :15,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :16,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :17,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :18,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :19,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :20,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :21,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :22,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     },
+//     {
+//         id :23,
+//         imgsrc : "",
+//         desc : " ",
+//         title : " "
+//     }    
+// ]
+
+// data.map((item)=> {
+//     console.log(`ID: ${item.id}`);
+// })
+
